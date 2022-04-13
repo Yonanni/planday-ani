764fd567daba583589eb90d906f2d7b97b82d015
Yonanni <adhanamengisteab@gmail.com>
2022-04-13 12:04:05 +0200

    loader

--- src/components/LoadingView.tsx
+++ src/components/LoadingView.tsx
@@ -5,7 +5,7 @@ import planday from "../assets/planda.gif"
 const LoadingScreen = () => {
   return (
     // <Container className="mt-auto mb-auto mr-auto ml-auto">
-      <div className="mt-auto mb-auto mr-auto ml-auto">
+      <div className="planday">
         <img
           src={planday}
           alt="planday logo"
